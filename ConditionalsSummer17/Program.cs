2462435afb45336308dc0470811298cd19dbbb3e
Joe Santoiemmo <JSanto333@gmail.com>
2017-06-28 12:04:33 -0400

    Initial commit

--- ConditionalsSummer17/Program.cs
+++ ConditionalsSummer17/Program.cs
@@ -10,6 +10,32 @@ namespace ConditionalsSummer17
     {
         static void Main(string[] args)
         {
+            double totalBill;
+            double grandTotal;
+
+            Console.WriteLine("How many diners are there?");
+            int peopleDining = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much is each diner paying?");
+            double costPerDiner = double.Parse(Console.ReadLine());
+
+            totalBill = peopleDining * costPerDiner;
+
+            if (totalBill >= 50)
+            {
+                double billDiscount = (totalBill * .10);
+                grandTotal = (totalBill - billDiscount);
+                Console.WriteLine("Congratulations, you will receive a 10% discount");
+                Console.WriteLine("Your total bill is " + grandTotal);
+            }
+            else
+            {
+                double billDiscount = (totalBill * .05);
+                grandTotal = (totalBill - billDiscount);
+                Console.WriteLine("Congratulations, you will receive a 5% discount");
+                Console.WriteLine("Your total bill is " + grandTotal);
+                
+            }
         }
     }
 }
